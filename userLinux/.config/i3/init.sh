4117a11e1cc34515dc45093313db18bb3ffdab40
Cody Duong <duongcody507@gmail.com>
2021-05-28 16:45:14 -0500

    Update init.sh

--- userLinux/.config/i3/init.sh
+++ userLinux/.config/i3/init.sh
@@ -1,2 +1,7 @@
 xrandr --output DP-0 --primary --right-of DVI-D-0
 xrandr --output HDMI-0 --below DP-0
+
+i3-msg 'workspace "1"; exec /usr/bin/gnome-terminal'
+i3-msg 'workspace "1"; exec code' && sleep 1
+i3-msg 'workspace "3"; exec /usr/bin/discord' && sleep 1
+i3-msg 'workspace "2"; exec firefox'
