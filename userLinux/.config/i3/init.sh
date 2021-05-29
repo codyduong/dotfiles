9206007a6ae5d2712bda76ed8e4b34c1dae77c2f
Cody Duong <duongcody507@gmail.com>
2021-05-29 11:39:14 -0500

    Present Main-PC Config

--- userLinux/.config/i3/init.sh
+++ userLinux/.config/i3/init.sh
@@ -2,6 +2,6 @@ xrandr --output DP-0 --primary --right-of DVI-D-0
 xrandr --output HDMI-0 --below DP-0
 
 i3-msg 'workspace "1"; exec /usr/bin/gnome-terminal'
-i3-msg 'workspace "1"; exec code' && sleep 1
-i3-msg 'workspace "3"; exec /usr/bin/discord' && sleep 1
+i3-msg 'workspace "1"; exec code' && sleep 3
+i3-msg 'workspace "3"; exec /usr/bin/discord' && sleep 3
 i3-msg 'workspace "2"; exec firefox'
