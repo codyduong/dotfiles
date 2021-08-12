92e238b0c3b3126ce041a8db67761fd42bb36fe3
Cody Duong <duongcody507@gmail.com>
2021-08-12 17:21:37 -0500

    Update init.sh

--- userLinux/.config/i3/init.sh
+++ userLinux/.config/i3/init.sh
@@ -1,12 +1,6 @@
 xrandr --output DP-0 --primary --right-of DVI-D-0
 xrandr --output HDMI-0 --below DP-0
 
-workspace 1 output DP-0
-workspace 2 output DP-0
-workspace 3 output DVI-D-0
-workspace 4 output DVI-D-0
-workspace 0 output HDMI-0
-
 i3-msg 'workspace "1"; exec /usr/bin/gnome-terminal'
 i3-msg 'workspace "2"; exec code' && sleep 3
 i3-msg 'workspace "0"; exec /usr/bin/discord' && sleep 3
