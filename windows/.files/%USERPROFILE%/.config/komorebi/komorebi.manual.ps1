e407b4f60048f665053a253d5feb46ecb52ebe06
Cody Duong <duongcody507@gmail.com>
2024-03-17 13:28:31 -0500

    Add changes

--- windows/.files/%USERPROFILE%/.config/komorebi/komorebi.manual.ps1
+++ windows/.files/%USERPROFILE%/.config/komorebi/komorebi.manual.ps1
@@ -6,3 +6,14 @@ komorebic.exe float-rule exe "steamwebhelper.exe"
 
 # hizashi
 komorebic.exe float-rule exe "hizashi.exe"
+
+# git-credential-manager popup
+komorebic.exe float-rule exe "git-credential-manager.exe"
+
+# fusion 360
+# komorebic.exe float-rule title "Save"
+komorebic.exe float-rule class "Qt624QWindowIcon"
+
+# ultimaker cura
+komorebic.exe float-rule title "Abort print"
+komorebic.exe float-rule class "Qt5152QWindowIcon"
