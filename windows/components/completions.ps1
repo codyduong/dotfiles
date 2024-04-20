2984e9a058ec02390ac151acb586e38e50292ed2
Cody Duong <duongcody507@gmail.com>
2024-04-20 15:42:17 -0500

    Fix completions

--- windows/components/completions.ps1
+++ windows/components/completions.ps1
@@ -1,6 +1,7 @@
 Get-ChildItem -Path (Join-Path $PSScriptRoot "completions")
 | Where-Object { $_.extension -eq ".ps1" }
 | Where-Object { $_.Name[0] -ne "." }
+# | Where-Object { $_.Name -notlike 'komorebic.ps1*'}
 | ForEach-Object -process {
   # Invoke-Expression ". '$_'"
   Write-Host "components.$_ : $(Measure-Command { Invoke-Expression ". '$_'" })"
