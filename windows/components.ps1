f7f6b38a95b48723ef862909822feb1c195c2a91
Cody Duong <duongcody507@gmail.com>
2024-04-26 20:38:41 -0500

    saving work

--- windows/components.ps1
+++ windows/components.ps1
@@ -2,9 +2,9 @@ Get-ChildItem -Path "./components"
 | Where-Object { $_.extension -eq ".ps1" }
 | Where-Object { $_.Name[0] -ne "." }
 | ForEach-Object -process {
-    # Invoke-Expression ". '$_'"
-    Write-Host "components.$_ : $(Measure-Command { Invoke-Expression ". '$_'" })"
-  }
+  # Invoke-Expression ". '$_'"
+  Write-Host "components.$_ : $(Measure-Command { Invoke-Expression ". '$_'" })"
+}
 
 # PS-Readline
 Import-Module PSProfiler
