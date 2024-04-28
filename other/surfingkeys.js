4add8a791978b002d37cc4a1ab2f15d3b80ffb31
Cody Duong <duongcody507@gmail.com>
2024-04-27 22:36:51 -0500

    Adds typescript-tools.nvim, .stylua.toml default
    * adds other for surfingkeys.js

---
+++ other/surfingkeys.js
@@ -0,0 +1,65 @@
+settings.theme = `
+.sk_theme {
+    font-family: Input Sans Condensed, Charcoal, sans-serif;
+    font-size: 10pt;
+    background: #24272e;
+    color: #abb2bf;
+}
+.sk_theme tbody {
+    color: #f9fafa#f9fafa;
+}
+.sk_theme input {
+    color: #d0d0d0;
+}
+.sk_theme .url {
+    color: #61afef;
+}
+.sk_theme .annotation {
+    color: #56b6c2;
+}
+.sk_theme .omnibar_highlight {
+    color: #528bff;
+}
+.sk_theme .omnibar_timestamp {
+    color: #e5c07b;
+}
+.sk_theme .omnibar_visitcount {
+    color: #98c379;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+    background: #303030;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+    background: #3e4452;
+}
+#sk_status, #sk_find {
+    font-size: 20pt;
+}
+
+#sk_tabs {
+    top: 10%;
+    transform: translate(50vw, 0) translate(-50%, 0);
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.8);
+    background: #24272e;
+}
+div.sk_tab {
+    background: inherit;
+    padding-left: 24px;
+    border-radius: 0;
+    box-shadow: none !important;
+}
+div.sk_tab:nth-child(odd) {
+    background: #303030
+}
+div.sk_tab:last-child {
+    border-bottom: solid 1px black;
+}
+div.sk_tab_title {
+    color: #f9fafa;
+}
+#sk_tabs.vertical div.sk_tab_hint {
+    left: 0;
+    background: none;
+    color: #f9fafa;
+}
+`;
