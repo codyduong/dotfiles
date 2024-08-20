a4a9738a818ee7f771e19663d374af2e3d767687
Cody Duong <duongcody507@gmail.com>
2024-08-20 02:41:33 -0500

    saving

--- windows/.files/%LOCALAPPDATA%/Microsoft/PowerToys/Keyboard Manager/README.md
+++ windows/.files/%LOCALAPPDATA%/Microsoft/PowerToys/Keyboard Manager/README.md
@@ -4,10 +4,13 @@
 "%LOCALAPPDATA%\Microsoft\PowerToys\Keyboard Manager"
 -->
 
-| Mapped From                                  | Mapped To                    | Reason                                                      |
-| -------------------------------------------- | ---------------------------- | ----------------------------------------------------------- |
-| <kbd>Win</kbd>+<kbd>Shift</kbd>+<kbd>W</kbd> | <kbd>Alt</kbd>+<kbd>F4</kbd> | The bind used to close windows on my i3 Linux Configuration |
-| <kbd>Win</kbd>+<kbd>D</kbd>                  | <kbd>Alt</kbd>+<kbd>D</kbd>  | dmenu                                                       |
-| <kbd>CapsLock</kbd>                          | <kbd>F13</kbd>               | Who the fuck uses caps-lock in 2024                         |
-| <kbd>Win</kbd>+<kbd>M</kbd>                  | Disabled                     | Disables minimize shortcut                                  |
-| <kbd>Win</kbd>+<kbd>F</kbd>                  | Disabled                     | Disables feedback hub                                       |
+| Mapped From                                   | Mapped To                    | Reason                                                      |
+| --------------------------------------------- | ---------------------------- | ----------------------------------------------------------- |
+| <kbd>Win</kbd>+<kbd>Shift</kbd>+<kbd>W</kbd>  | <kbd>Alt</kbd>+<kbd>F4</kbd> | The bind used to close windows on my i3 Linux Configuration |
+| <kbd>Win</kbd>+<kbd>D</kbd>                   | <kbd>Alt</kbd>+<kbd>D</kbd>  | dmenu                                                       |
+| <kbd>CapsLock</kbd>                           | <kbd>F13</kbd>               | Who the fuck uses caps-lock in 2024                         |
+| <kbd>Win</kbd>Ctrl<kbd>D</kbd>                | Disabled                     | Disable virtual desktop creation                            |
+| <kbd>Win</kbd>+<kbd>M</kbd>                   | Disabled                     | Disable minimize shortcut                                   |
+| <kbd>Win</kbd>+<kbd>F</kbd>                   | Disabled                     | Disable feedback hub                                        |
+<!-- | <kbd>Alt</kbd>+<kbd>Tab</kbd>                 | Disabled                     | Disable tab switching                                       |
+| <kbd>Ctrl</kbd>+<kbd>Tab</kbd>+<kbd>Tab</kbd> | Disabled                     | Disable tab viewing                                         | -->
--- windows/.files/%USERPROFILE%/.config/hitokage/init.lua
+++ windows/.files/%USERPROFILE%/.config/hitokage/init.lua
@@ -3,22 +3,182 @@ local monitors = hitokage.monitor.get_all()
 --- @type BarArray
 local bars = {}
 
+local reactive_labels = {}
+local reactive_imgs = {}
+
+local clock_icons = {
+  "\u{F144A}",
+  "\u{F143F}",
+  "\u{F1440}",
+  "\u{F1441}",
+  "\u{F1442}",
+  "\u{F1443}",
+  "\u{F1444}",
+  "\u{F1445}",
+  "\u{F1446}",
+  "\u{F1447}",
+  "\u{F1448}",
+  "\u{F1449}",
+}
+
+--- @type table<number, ReactiveString>
+local reactive_clock_icons = {}
+
 for _, monitor in ipairs(monitors) do
-  if (monitor.model == "LG SDQHD") then
+  if monitor.model == "LG SDQHD" then
     goto continue
   end
 
-  table.insert(bars, hitokage.bar.create(monitor, {
-    widgets = {
-      { Box = {} },
-      { Workspace = { halign = "Center", item_height = 22, item_width = 22 } },
-      { Clock = { format = "%a %b %u %r", halign = 'End' } },
-    },
-    width = monitor.geometry.width - 16,
-    offset = {
-      x = 8,
-      y = 8,
-    }
-  }))
+  -- TODO better idiomatic syntax
+  -- monitor.create_bar({
+  --   widgets = {
+  --     {Workspace = {}},
+  --     {Clock = {format = "%Y-%m-%d %H:%M:%S"}},
+  --     {Box = {}},
+  --   }
+  -- })
+
+  -- the unsafe operation occurs in creating reactives in lua. this has to do with how we serialize data...
+  local reactive_label = hitokage.unstable.reactive.create("foo \u{EECB}")
+  local reactive_img = hitokage.unstable.reactive.create("./smiley.png")
+  local reactive_clock_icon = hitokage.unstable.reactive.create(clock_icons[tonumber(os.date("%H")) % 12 + 1])
+
+  table.insert(reactive_labels, reactive_label)
+  table.insert(reactive_imgs, reactive_img)
+  table.insert(reactive_clock_icons, reactive_clock_icon)
+
+  local mem_str =
+  '{{pad "left" (round (div used 1024) 1) 4}} ({{ pad "left" (concat (round (mult (div used total) 100) 1) "%") 4 }})'
+  local cpu_str = '{{pad "left" (concat (round (mult usage 100) 1) "%") 6}}'
+
+  -- .. 'C1: {{pad "right" (concat (round (mult core1_usage 100) 1) "%") 6}}'
+  -- .. 'C1: {{pad "right" (concat (round (mult core1_usage 100) 1) "%") 6}}'
+  -- .. 'C2: {{pad "right" (concat (round (mult core2_usage 100) 1) "%") 6}}'
+  -- .. 'C3: {{pad "right" (concat (round (mult core3_usage 100) 1) "%") 6}}'
+  -- .. 'C4: {{pad "right" (concat (round (mult core4_usage 100) 1) "%") 6}}'
+  -- .. 'C5: {{pad "right" (concat (round (mult core5_usage 100) 1) "%") 6}}'
+  -- .. 'C6: {{pad "right" (concat (round (mult core6_usage 100) 1) "%") 6}}'
+  -- .. 'C7: {{pad "right" (concat (round (mult core7_usage 100) 1) "%") 6}}'
+
+  table.insert(
+    bars,
+    hitokage.bar.create(monitor, {
+      widgets = {
+        {
+          Box = {
+            halign = 'Start',
+            hexpand = true,
+            widgets = {
+              {
+                Box = {
+                  class = "left bar-group",
+                  halign = "Start",
+                  widgets = {
+                    { Workspace = { halign = "Start", item_height = 24, item_width = 24, format = "{{add index 1}}" } },
+                  }
+                }
+              },
+              -- { Label = { label = "\u{E0B4}", class = "bar-group-extra" } },
+            }
+          }
+        },
+        {
+          Box = {
+            halign = 'Center',
+            hexpand = true,
+            widgets = {
+              -- { Label = { label = "\u{E0B6}", class = "bar-group-extra fix-spacing" } },
+              {
+                Box = {
+                  homogeneous = false,
+                  class = "center bar-group",
+                  widgets = {
+                    { Label = { class = "icon clock", label = "\u{F00ED}", } },
+                    { Clock = { class = "data", format = "%a %b %e", halign = "End" } },
+                    { Label = { class = "icon clock", label = reactive_clock_icon, } },
+                    { Clock = { class = "data", format = "%r", halign = "End" } },
+                  }
+                }
+              },
+              -- { Label = { label = "\u{E0B4}", class = "bar-group-extra" } },
+            }
+          }
+        },
+        {
+          Box = {
+            halign = 'End',
+            hexpand = true,
+            widgets = {
+              {
+                Box = {
+                  halign = 'End',
+                  hexpand = true,
+                  widgets = {
+                    -- { Label = { halign = "End", label = "\u{E0B6}", class = "bar-group-extra" } },
+                    {
+                      Box = {
+                        halign = "End",
+                        class = "right bar-group",
+                        widgets = {
+
+                          { Label = { class = "icon memory", label = "\u{EFC5}", } },
+                          { Memory = { class = "data", format = mem_str, halign = "End" } },
+                          { Label = { class = "icon", label = "\u{F4BC}" } },
+                          { Cpu = { class = "data", format = cpu_str, halign = "End" } },
+                        },
+                      }
+                    },
+                  }
+                }
+              },
+            }
+          }
+
+        },
+      },
+      homogeneous = true,
+    })
+  )
   ::continue::
 end
+
+--- @alias WorkspaceTable table<number, Workspace>
+--- @type WorkspaceTable
+local workspaces = {}
+
+--- @alias ClockTable table<number, Clock>
+--- @type ClockTable
+local clocks = {}
+
+--- @alias BoxesTable table<number, Box>
+--- @type BoxesTable
+local boxes = {}
+
+for i, bar in ipairs(bars) do
+  while not bar:is_ready() do
+    hitokage.debug("waiting for bar to instantiate", i)
+    coroutine.yield() -- yield to other processes to occur
+  end
+  for _, widget in ipairs(bar:get_widgets()) do
+    hitokage.debug(widget)
+    if widget.type == "Clock" then
+      table.insert(clocks, widget)
+    end
+    if widget.type == "Workspace" then
+      table.insert(workspaces, widget)
+    end
+    if widget.type == "Box" then
+      table.insert(boxes, widget)
+    end
+  end
+end
+
+local update_clock_icon = hitokage.timeout(1000, function()
+  for _, clock_icon in ipairs(reactive_clock_icons) do
+    local hour_24 = tonumber(os.date("%H"))
+    local hour_12 = hour_24 % 12
+    clock_icon:set(clock_icons[hour_12 + 1])
+  end
+end)
+
+hitokage.dispatch(update_clock_icon)
