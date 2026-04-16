9b3a2e64273cae68c4490f591e69d7dc7e27b157
Cody Duong <duongcody507@gmail.com>
2026-04-16 18:14:02 -0500

    fix: rustup install

--- termux/bootstrap.sh
+++ termux/bootstrap.sh
@@ -88,12 +88,15 @@ install_pkg zoxide
 # 3. Rust (via rustup)
 # ---------------------------------------------------------------------------
 
+export CARGO_HOME="$HOME/.cargo"
+export RUSTUP_HOME="$HOME/.rustup"
+export PATH="$CARGO_HOME/bin:$PATH"
+
 if has rustup; then
   warn "rustup already installed"
 else
   info "Installing Rust via rustup ..."
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
-  export PATH="$HOME/.cargo/bin:$PATH"
   ok "Rust $(rustc --version) installed"
 fi
 
